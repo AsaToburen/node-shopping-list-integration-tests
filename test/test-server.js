d06414f84c7fd017baf6303ef0cf1a9ca6534b26
Asa Toburen <atopettit@gmail.com>
2018-01-13 11:23:13 -0700

    adding integration tests for recipes

--- test/test-server.js
+++ test/test-server.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-const {app, runServer, closeServer} = require('../server');
+const { app, runServer, closeServer } = require('../server');
 
 // this lets us use *expect* style syntax in our tests
 // so we can do things like `expect(1 + 1).to.equal(2);`
@@ -13,9 +13,7 @@ const expect = chai.expect;
 // see: https://github.com/chaijs/chai-http
 chai.use(chaiHttp);
 
-
 describe('Shopping List', function() {
-
   // Before our tests run, we activate the server. Our `runServer`
   // function returns a promise, and we return the that promise by
   // doing `return runServer`. If we didn't return a promise here,
@@ -43,7 +41,8 @@ describe('Shopping List', function() {
     // we must either return a Promise object or else call a `done` callback
     // at the end of the test. The `chai.request(server).get...` call is asynchronous
     // and returns a Promise, so we just return it.
-    return chai.request(app)
+    return chai
+      .request(app)
       .get('/shopping-list')
       .then(function(res) {
         expect(res).to.have.status(200);
@@ -67,8 +66,9 @@ describe('Shopping List', function() {
   //  2. inspect response object and prove it has right
   //  status code and that the returned object has an `id`
   it('should add an item on POST', function() {
-    const newItem = {name: 'coffee', checked: false};
-    return chai.request(app)
+    const newItem = { name: 'coffee', checked: false };
+    return chai
+      .request(app)
       .post('/shopping-list')
       .send(newItem)
       .then(function(res) {
@@ -79,7 +79,7 @@ describe('Shopping List', function() {
         expect(res.body.id).to.not.equal(null);
         // response should be deep equal to `newItem` from above if we assign
         // `id` to it from `res.body.id`
-        expect(res.body).to.deep.equal(Object.assign(newItem, {id: res.body.id}));
+        expect(res.body).to.deep.equal(Object.assign(newItem, { id: res.body.id }));
       });
   });
 
@@ -100,28 +100,32 @@ describe('Shopping List', function() {
       checked: true
     };
 
-    return chai.request(app)
-      // first have to get so we have an idea of object to update
-      .get('/shopping-list')
-      .then(function(res) {
-        updateData.id = res.body[0].id;
-        // this will return a promise whose value will be the response
-        // object, which we can inspect in the next `then` block. Note
-        // that we could have used a nested callback here instead of
-        // returning a promise and chaining with `then`, but we find
-        // this approach cleaner and easier to read and reason about.
-        return chai.request(app)
-          .put(`/shopping-list/${updateData.id}`)
-          .send(updateData);
-      })
-      // prove that the PUT request has right status code
-      // and returns updated item
-      .then(function(res) {
-        expect(res).to.have.status(200);
-        expect(res).to.be.json;
-        expect(res.body).to.be.a('object');
-        expect(res.body).to.deep.equal(updateData);
-      });
+    return (
+      chai
+        .request(app)
+        // first have to get so we have an idea of object to update
+        .get('/shopping-list')
+        .then(function(res) {
+          updateData.id = res.body[0].id;
+          // this will return a promise whose value will be the response
+          // object, which we can inspect in the next `then` block. Note
+          // that we could have used a nested callback here instead of
+          // returning a promise and chaining with `then`, but we find
+          // this approach cleaner and easier to read and reason about.
+          return chai
+            .request(app)
+            .put(`/shopping-list/${updateData.id}`)
+            .send(updateData);
+        })
+        // prove that the PUT request has right status code
+        // and returns updated item
+        .then(function(res) {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('object');
+          expect(res.body).to.deep.equal(updateData);
+        })
+    );
   });
 
   // test strategy:
@@ -129,13 +133,91 @@ describe('Shopping List', function() {
   //  to delete.
   //  2. DELETE an item and ensure we get back a status 204
   it('should delete items on DELETE', function() {
-    return chai.request(app)
-      // first have to get so we have an `id` of item
-      // to delete
-      .get('/shopping-list')
+    return (
+      chai
+        .request(app)
+        // first have to get so we have an `id` of item
+        // to delete
+        .get('/shopping-list')
+        .then(function(res) {
+          return chai.request(app).delete(`/shopping-list/${res.body[0].id}`);
+        })
+        .then(function(res) {
+          expect(res).to.have.status(204);
+        })
+    );
+  });
+});
+
+describe('Recipes', function() {
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  it('should list items on GET', function() {
+    return chai
+      .request(app)
+      .get('/recipes')
       .then(function(res) {
-        return chai.request(app)
-          .delete(`/shopping-list/${res.body[0].id}`);
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('array');
+        expect(res.body.length).to.be.at.least(1);
+        const expectedKeys = ['name', 'ingredients'];
+        res.body.forEach(function(item) {
+          expect(item).to.include.keys(expectedKeys);
+        });
+      });
+  });
+
+  it('should add an item on POST', function() {
+    const newItem = { name: 'Cookies', ingredients: ['sugar', 'milk', 'flour'] };
+    return chai
+      .request(app)
+      .post('/recipes')
+      .send(newItem)
+      .then(function(res) {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.include.keys('id', 'name', 'ingredients');
+        expect(res.body).to.be.a('object');
+        expect(res.body.id).to.not.equal(null);
+        expect(res.body).to.deep.equal(Object.assign(newItem, { id: res.body.id }));
+      });
+  });
+
+  it('should update items on PUT', function() {
+    const updateData = {
+      name: 'brownies',
+      ingredients: ['chocolate', 'sugar', 'flour']
+    };
+
+    return chai
+      .request(app)
+      .get('/recipes')
+      .then(function(res) {
+        updateData.id = res.body[0].id;
+        return chai
+          .request(app)
+          .put(`/recipes/${updateData.id}`)
+          .send(updateData);
+      })
+      .then(function(res) {
+        expect(res.body).to.be.a('object');
+        expect(res).to.have.status(204);
+      });
+  });
+
+  it('should delete items on DELETE', function() {
+    return chai
+      .request(app)
+      .get('/recipes')
+      .then(function(res) {
+        return chai.request(app).delete(`/recipes/${res.body[0].id}`);
       })
       .then(function(res) {
         expect(res).to.have.status(204);
